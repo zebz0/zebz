4e05bae4339b135705187a6278ea291a259c3ad3
zebz0 <160431310+zebz0@users.noreply.github.com>
2024-05-03 14:36:19 +0300

    Update lok.py

--- lok.py
+++ lok.py
@@ -96,11 +96,11 @@ def cc():
     
     D = '_'
     def random_user():
-        a1 = random.choice(D)
-        a2 = random.choice(A)
+        a1 = random.choice(A)
+        a2 = random.choice(D)
         a3 = random.choice(A)
-        a4 = random.choice(A)
-        a5 = random.choice(D)
+        a4 = random.choice(D)
+        a5 = random.choice(A)
         return a1 + a2 + a3 + a4 + a5
 
     while True:
